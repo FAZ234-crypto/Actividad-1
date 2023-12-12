7a6551312b7d713d90d4e96646093f69b4d56449
FAZ234-crypto <60855216+FAZ234-crypto@users.noreply.github.com>
2023-12-12 09:19:03 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# 1 ejercicio operaciones basicas
+
+suma = 4 + 3
+resta = 45.3 - 5.4
+multipl = 25 * 8
+div = 50.5 / 3.4
+print("\nsuma :", suma, "\nresta :", resta, "\nmultiplicacion :", multipl, "\ndivision :", div)
+
+# 2 ejercicio
+nombre = 'Joseph Ariel Astudillo Pineda'
+longitud = len(nombre)
+print(nombre, "\nlongitud:", longitud)
+'''
+Este programa imprime el nombre completo y su longitud
+'''
+
+# 3 ejercicio
+entera = 34
+flotante = 56.5
+
+nueva1 = float(entera)
+nueva2 = int(flotante)
+print("\nEntera : ", entera, "\nFlotante :", flotante, "\nConversion1: ", nueva1, "\nConversion2: ", nueva2)
+"Esta parte del programa mostrara en pantalla dos variables numericas, luego una conversion de estas mismas"
+
